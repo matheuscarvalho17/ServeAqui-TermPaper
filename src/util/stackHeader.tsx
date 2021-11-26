efa3197c0eb236bde98eda8f2c44614215a742d2
MatheusCarvalho <matheusinholopescarvalho@gmail.com>
2021-11-26 16:18:38 -0300

    stackHeader - Ajuste no nome da mesa

--- src/util/stackHeader.tsx
+++ src/util/stackHeader.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import colors from '../customs/colors';
 import {StyleSheet} from 'react-native';
-import {Appbar} from 'react-native-paper';
 import {useAppContext} from './context';
+import {Appbar} from 'react-native-paper';
 import {setFontSizeValue, setHeightValue} from './ajustScreen';
 
 const StackHeader = ({scene, navigation}) => {
@@ -22,10 +22,12 @@ const StackHeader = ({scene, navigation}) => {
 			<Appbar.Content title={title} titleStyle={styles.title} />
 			{!navigation.canGoBack() && (
 				<>
-					<Appbar.Content
-						titleStyle={styles.title}
-						title={'Mesa ' + actualTable.name}
-					/>
+					{actualTable && (
+						<Appbar.Content
+							titleStyle={styles.title}
+							title={'Mesa ' + actualTable.name}
+						/>
+					)}
 					<Appbar.Action
 						onPress={() => {}}
 						icon="dots-vertical"
